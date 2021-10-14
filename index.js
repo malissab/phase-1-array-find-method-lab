74b889153b60947a9679db593610848b577ba86a
Malissa Bishop <malissambishop7@gmail.com>
2021-10-14 10:27:59 -0700

    commit tree bde9dec7aee9e025653d7229f53538d851c955b4

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-
+function isWin (record) {
+     return record.result === 'W'
+}
+const superbowlWin = record => {
+     let win = record.find(isWin)
+     if (win) {
+          return win.year
+     } else {
+          return win
+     }
+}
